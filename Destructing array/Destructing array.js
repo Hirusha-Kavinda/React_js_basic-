0eb241f93e480b6bc53a689cd8c584cb5c2745d7
Hirusha-Kavinda <hirusha631@gmail.com>
2024-02-01 12:20:30 +0530

    destructing array

---
+++ Destructing array/Destructing array.js
@@ -0,0 +1,15 @@
+// array destructing
+
+// get the first value in array
+let [firstlanguage] = ['python','java','javascript']
+console.log(firstlanguage); 
+
+
+// get the first value in array
+let [, seclanguage] = ['python','java','javascript']
+console.log(seclanguage); 
+
+
+// get the first value in array
+let [, , thirdlanguage] = ['python','java','javascript']
+console.log(thirdlanguage); 
