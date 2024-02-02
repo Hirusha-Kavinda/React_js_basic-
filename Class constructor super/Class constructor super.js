a2e7f0dc29c374abc0d3bfdfa31881ad4b78b9c7
Hirusha-Kavinda <hirusha631@gmail.com>
2024-02-02 17:13:58 +0530

    constructors

--- Class constructor super/Class constructor super.js
+++ Class constructor super/Class constructor super.js
@@ -12,11 +12,11 @@ Holiday.prototype.info = function(){
 
 var nepal = new  Holiday('Nepal' , 30);
 console.log(nepal.info())
-
+    
 
 
 // using class and constructors
-
+// super class
 class Holiday2 {
     constructor(destination, days){
         this.destination = destination
@@ -33,3 +33,18 @@ console.log(trip.info())
 
 
 // extent one class to other class
+// sub class
+class expedition extends Holiday2 {
+    constructor(destination, days, gear){
+        super(destination, days)
+             this.gear = gear;
+    }
+
+    info(){ 
+        super.info();
+        console.log(`Bring your ${this.gear.join(" and you")}`)       
+    }
+}
+
+const tripWithGear = new expedition('Everest' , 30 , ['Sunglasses','Flags','Camera']);
+tripWithGear.info();
