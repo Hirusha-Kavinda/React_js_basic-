a5f45ef0d44478ae619c90e1e41c51855fbca230
Hirusha-Kavinda <hirusha631@gmail.com>
2024-05-29 17:56:42 +0530

    add test props

---
+++ dynamic_content/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Topic from './topic';
+
+function App() {
+  return (
+    <div>
+      <Topic language="java" />
+      <Topic language="python"/>
+      <Topic language="c++"/>
+      <Topic language="type Script"/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ dynamic_content/src/topic.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Topic(props) {
+  return (
+    <div>
+      <h3>This is a peo {props.language} </h3>
+    </div>
+  );
+}
+
+export default Topic;
